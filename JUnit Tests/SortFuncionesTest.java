5c86492536d8fc880224866937f8a9e0e890a78f
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-02-12 21:39:28 -0600

    Add files via upload

---
+++ JUnit Tests/SortFuncionesTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+/**
+ * @author Usuario Dell
+ *
+ */
+class SortFuncionesTest {
+	iSortFunciones funciones = new iSortFunciones();
+	int[] list = {2,3,1,5,4};
+	int[] list1 = {2,3,1,5,4};
+	int[] listO = {1,2,3,4,5};
+	String listaOrdenada = Arrays.toString(listO);
+	
+	@Test
+	public void SelectionSortTest() {
+		assertEquals(listaOrdenada, Arrays.toString(funciones.SelectionSort(list)));
+		}
+	@Test	
+	public void MergeSortTest() {
+		assertEquals(listaOrdenada, Arrays.toString(funciones.MergeSort(list)));
+	}
+	@Test
+	public void QuickSortTest() {
+		funciones.QuickSort(list1);
+		assertEquals(listaOrdenada, Arrays.toString(list1));
+	}
+	@Test
+	public void RadixSortTest() {
+		assertEquals(listaOrdenada, Arrays.toString(funciones.RadixSort(list)));
+	}
+	
+	
+	
+}
+
